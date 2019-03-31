314ac7a7816ba569a9482d753200b1d028f42070
RSMJr <journal.indoor@gmail.com>
2019-03-31 15:05:03 -0300

    Parte 2 - Cat Cliker Duo

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Javascript Design Pattern
 
-Para melhorar meus conhecimentos
+Para melhorar meus conhecimentos com Design Patterns
+
+## Parte 1 - Cat Clicker
+
+Fazer um template com uma foto de gato. Ao clicar na foto incrementar um contador.
+
+## Parte 2 - Cat Clicker Duo
+
+Adicionar mais uma foto de gato, dessa vez mostrando o nome do gato acima da foto e o número de cliques em algum lugar da tela. Ao clicar na foto, incrementar o contador.
+
+Obs.: Inserir o nome do gato através de Javascript, sem escrever direto no HTML.
--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -1,6 +1,24 @@
-var fotoAmora = document.getElementById('foto-amora');
+var fotoAmora = document.getElementsByClassName('foto-amora');
+
 var contador = 0;
-fotoAmora.addEventListener('click', function(){
-    contador++;
-    console.log("Vc clicou: " + contador + "x");
-},false);
+var cxContador = document.createElement("h2");
+cxContador.className = "contador-cliques";
+document.body.appendChild(cxContador);
+
+
+for(i=0;i<fotoAmora.length;i++){
+    fotoAmora[i].addEventListener('click', function(){
+        cxContador.innerHTML = contador++;
+    });
+}
+
+function nomeGatinha(){
+    let nomes = ["Amora Jr", "Amora Tepha"];
+    for(i=0;i<fotoAmora.length;i++){
+        let cxNome = document.createElement("h3");
+        cxNome.className = "nome-gatinha";
+        cxNome.innerText = nomes[i];
+        fotoAmora[i].parentElement.insertBefore(cxNome,fotoAmora[i]);
+    }
+}
+nomeGatinha();
