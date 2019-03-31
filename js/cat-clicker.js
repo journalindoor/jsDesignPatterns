55a8a17c600a24db1547104604a7756360e515e9
RSMJr <journal.indoor@gmail.com>
2019-03-31 15:48:44 -0300

    Ajuste no Cat Clicker Duo
    Era para ser um contador individual para cada gatinha

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -1,24 +1,33 @@
 var fotoAmora = document.getElementsByClassName('foto-amora');
-
 var contador = 0;
-var cxContador = document.createElement("h2");
-cxContador.className = "contador-cliques";
-document.body.appendChild(cxContador);
 
+for (i = 0; i < fotoAmora.length; i++) {
+    fotoAmora[i].addEventListener('click', function () {
 
-for(i=0;i<fotoAmora.length;i++){
-    fotoAmora[i].addEventListener('click', function(){
-        cxContador.innerHTML = contador++;
+        let contadorIndividual = this.parentElement.getElementsByClassName('contador-cliques');
+        contadorIndividual[0].innerText++;        
+        
     });
 }
 
-function nomeGatinha(){
+function nomeGatinha() {
     let nomes = ["Amora Jr", "Amora Tepha"];
-    for(i=0;i<fotoAmora.length;i++){
-        let cxNome = document.createElement("h3");
+    for (i = 0; i < fotoAmora.length; i++) {
+        let cxNome = document.createElement("h2");
         cxNome.className = "nome-gatinha";
         cxNome.innerText = nomes[i];
-        fotoAmora[i].parentElement.insertBefore(cxNome,fotoAmora[i]);
+        fotoAmora[i].parentElement.insertBefore(cxNome, fotoAmora[i]);
     }
 }
+function mostraContador(){
+    for (i = 0; i < fotoAmora.length; i++) {
+        let cxContador = document.createElement("h2");
+        cxContador.className = "contador-cliques";
+        cxContador.innerText = contador;
+        fotoAmora[i].parentElement.insertBefore(cxContador, fotoAmora[i].nextSibling);
+    }
+}
+
+
 nomeGatinha();
+mostraContador();
